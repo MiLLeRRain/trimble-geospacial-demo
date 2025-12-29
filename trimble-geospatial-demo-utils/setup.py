e738b1eb8f0cec061c9ce6710e65fb4fc2998155
MiLLeRRain <xxdezz@gmail.com>
2025-12-30 11:03:40 +1300

    Add utility functions for site lock management and tiling configuration

---
+++ trimble-geospatial-demo-utils/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name="trimble-geospatial-demo-utils",
+    version="0.1.0",
+    description="Trimble geospatial demo utility helpers.",
+    packages=["trimble_geospatial_demo_utils"],
+    package_dir={"trimble_geospatial_demo_utils": "."},
+    python_requires=">=3.8",
+    install_requires=[],
+)
